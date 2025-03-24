3be0bd48de0cbead6aa98609d0f71c52ee8348f0
suhyeon 999 <jsh53070@gmail.com>
2025-03-24 02:06:14 +0000

    구구단 만들기

---
+++ gugu.py
@@ -0,0 +1,9 @@
+# 사용자에게 몇 단을 출력할지 입력받기
+dan = int(input("몇 단을 출력할까요? "))
+
+# 구구단 출력
+print(f"== {dan}단 ==")
+for i in range(1, 10):
+    result = dan * i
+    print(f"{dan} × {i} = {result}")
+    
