9a31e160545a6b169f2a45eba46aa71b2f4fc132
suhyeon 999 <jsh53070@gmail.com>
2025-03-24 03:24:30 +0000

    자료형 및 연산자를 활용

---
+++ test.py
@@ -0,0 +1,9 @@
+# dan = int(input("몇 단을 출력할까요? "))
+dan = input("몇 단을 출력할까요? ")
+print(f"(s_dan)단")
+# print(f"== {dan}단 ==")
+# print(f"(dan)단, (dan)단, (dan)단")
+# number 값이 문자열이기 때문에 숫자로 변경해서 연산
+dan = int(s_dan)
+result = dan * 2
+print(result)
