f4284abf5820301fbf3d4a7c3d48cff1bcc06b08
suhyeon 999 <jsh53070@gmail.com>
2025-04-07 03:27:33 +0000

    myif 과제

--- myif.py
+++ myif.py
@@ -1,63 +1,75 @@
 # 아스키 코드 그림 출력 하기
 def print_cat():
-    cat_art = r"""
-      /\_/\  
-     ( o.o ) 
-     > ^ <
-    """
-    print(cat_art)
+    cat = [
+        " /\\_/\\  ",
+        "( o.o ) ",
+        " > ^ <  "
+    ]
+    
+    for line in cat:
+        print(line)
 
-print_cat()
 def print_dog():
-    dog_art = r"""
-       / \__
-      (    @\___
-      /         O
-     /   (_____/
-    /_____/   U
-    """
-    print(dog_art)
+    dog = [
+        " / \\__",
+        "(    @\\___",
+        " /         O",
+        "/   (_____/",
+        "/_____/   U"
+    ]
 
-print_dog()
+    for line in dog:
+        print(line)
+
 def print_rabbit():
-    rabbit_art = r"""
-     (\(\ 
-     ( -.-)
-     o_(")(")
-    """
-    print(rabbit_art)
+    rabbit = [
+        "  (\\_/)",
+        "  (o o)",
+        "  ( > )"
+    ]
+    
+    for line in rabbit:
+        print(line)
 
+# 여기 함수를 잘 수정해서 두개의 프로그램이 될 수 있게 만들어 보기기
 
-# 여기서 반복
-#for i range(5):
-while true: # 무한 반복 (계속 참)
+    def play():
     print("그림 출력 프로그램")
-    print("===========")
+    print("=====================")
     print("1. 고양이")
     print("2. 강아지")
-    print("3. 토끼")
-    print("===========")
-    n = int(input("선택(0을 입력하면 종료): "))
-    # 0이 입력되면 프로그램 종료
-    if n == 0:
-        print("프로그램을 종료합니다.")
-        break
+    print("3. 토 끼")
+    print("=====================")
+    # n = 3
+    n = int(input("선택: "))
+
     # 만약에 1을 입력하면 1번 캐릭터 출력
     if n == 1:
         print("고양이")
+        print_cat()
     # 만약에 2를 입력하면 2번 캐릭터 출력
     elif n == 2:
         print("강아지")
+        print_dog()
     # 3을 입력하면 3번 캐릭터 출력
     elif n == 3:
         print("토끼")
-    # 잘못 입력하면 잘못 입력했다고 출력
-    else :
-        print("잘못 입력")
-# 여기까지
+        print_rabbit()
+    # 잘못입력하면 잘못 입력했다고 출력
+    else:
+        print("잘못입력")    
 
- # 동물 그림 출력 프로그램이 총 5번 반복 실행될 수 있게 만드시오.
-# 위 프로그램을 할 수 있는 사람은 프로그램이 계속(무한)반복하게 하고
-# 만약에 0을 입력하면 종료 되는 프로그램을 만드시오
+# 동물 그림 출력 프로그램이 총 5번 반복 실행될 수 있게 만드시오.
+# 여기서 반복하면 되겠네
+print("5번 출력 프로그램 시작")
+for i in range(5):
+    play()
 
-
+print("5번 출력 프로그램 종료")
+# 위 프로그램을 완성한한 사람은 프로그램이 계속(무한)반복하게 하고
+# 만약에 0을 입력하면 종료 되는 프로그램을 만드시오.
+print("0을 입력하면 종료 프로그램 시작 ")
+while True: # 무한 반복 (계속 참)
+    play()
+# 여기까지 하면 되겠네
+print("0을 입력하면 종료 프로그램 종료 ")
---
+++ test4.py
@@ -0,0 +1,30 @@
+# 함수 만들기
+def hello():
+    print("hello world!")
+
+# 함수 호출(실행)
+hello()
+
+def hello_name(name):
+    print(f"안녕 {name}야~") 
+
+# 함수 호출(실행)
+name = input("이름을 입력: ")
+hello_name(name)
+
+# 연산을 하는 함수
+def cal(n1, n2, op): 
+    r = 0  # 결과값
+    if op == "+":
+        r = n1 + n2
+    elif op == "-":
+        r = n1 - n2
+    else:
+        print("잘못입력")
+    return r  # 결과값을 전달
+
+# 함수 호출 및 결과 출력
+r = cal(2, 1, "+")
+print(f"두 수를 더한 값: {r}")
+r = cal(2, 1, "-")
+print(f"두 수를 뺀 값: {r}")
